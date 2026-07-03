826a967e62194f20b2c585573926ab8ec790882d
masdin82-commits <masdin82@gmail.com>
2026-07-03 09:31:29 +0800

    Add files via upload

---
+++ src/components/cdr.tsx
@@ -0,0 +1,8 @@
+// src/components/cdr.tsx
+export default function cdr() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1>Cuan Dari Rumah!</h1>
+    </div>
+  );
+}
