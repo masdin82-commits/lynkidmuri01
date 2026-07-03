65625c8507a2d84b7ab0f2ba8a8a3af6a864cda2
masdin82-commits <masdin82@gmail.com>
2026-07-03 11:04:00 +0800

    Delete src directory

--- src/components/cdr.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/cdr.tsx
-export default function cdr() {
-  return (
-    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
-      <h1>Cuan Dari Rumah!</h1>
-    </div>
-  );
-}
